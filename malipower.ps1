b97bd68b202bccee446294e565d52ae84e4352f8
tokio-oyama <103776742+tokio-oyama@users.noreply.github.com>
2022-04-15 16:48:55 -0400

    Update malipower.ps1

--- malipower.ps1
+++ malipower.ps1
@@ -2,7 +2,7 @@ New-Item C:\Users\IEUser\AppData\Roaming\demotet\infected.txt
 Set-Content C:\Users\IEUser\AppData\Roaming\demotet\infected.txt 'You have been Infected!'
 
 $registryPath = "HKCU:\Software\Demotet\Scripts"
-$Name = "DemotetRegistry"
+$Name = "D7AD9WD56DQS89DW7DSAHDUQAI"
 $value = "1"
 
 IF(!(Test-Path $registryPath))
