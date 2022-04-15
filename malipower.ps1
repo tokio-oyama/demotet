c5f762e365710f097ffdb09efd8bc5aaab9707ac
tokio-oyama <103776742+tokio-oyama@users.noreply.github.com>
2022-04-15 16:09:01 -0400

    Update malipower.ps1

--- malipower.ps1
+++ malipower.ps1
@@ -1,3 +1,3 @@
-New-Item C:\Windows\System32\Demotet\infected.txt
-Set-Content C:\Windows\System32\Demotet\infected.txt 'You have been Infected!'
+New-Item C:\Users\IEUser\AppData\Roaming\demotet\infected.txt
+Set-Content C:\Users\IEUser\AppData\Roaming\demotet\infected.txt 'You have been Infected!'
 
